465ca0b9cdf9c29c5a994facfc7261efcd3e3a71
3278114689 <133768511+3278114689@users.noreply.github.com>
2023-06-04 20:12:13 +0800

    1

--- script.js
+++ script.js
@@ -3,14 +3,3 @@ document.getElementById('encouragement').addEventListener('click', function() {
     const randomIndex = Math.floor(Math.random() * encouragements.length);
     document.getElementById('message').innerText = encouragements[randomIndex];
 });
-function showImage() {
-    document.getElementById('fullScreenImage').style.display = 'block';
-    setTimeout(function() {
-      document.getElementById('fullScreenImage').style.display = 'none';
-    }, 2000);  // 2秒后隐藏图片
-  }
-  
-  // 假设“文本播放完毕”可以通过一些条件判断实现，当满足这个条件时调用showImage函数
-  if (encouragements) {
-    showImage();
-  }
