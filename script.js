c59ae6124a1305d1dc879f40391a4726529d6c2e
3278114689 <133768511+3278114689@users.noreply.github.com>
2023-06-09 16:09:03 +0800

    更新 script.js
    放弃

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('encouragement').addEventListener('click', function() {
-    const encouragements = ['邓丽菲，你是最棒的！', '相信自己，你可以的！', '你已经做得很好了!', '你的努力一定会有回报的！','今日幸运色：海洋蓝 幸运数字8'];
+    const encouragements = ['你是最棒的！', '相信自己，你可以的！', '你已经做得很好了!', '你的努力一定会有回报的！','今日幸运色：海洋蓝 幸运数字8'];
     const randomIndex = Math.floor(Math.random() * encouragements.length);
     document.getElementById('message').innerText = encouragements[randomIndex];
 });
