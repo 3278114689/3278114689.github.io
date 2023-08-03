da96c5b1ee124d0264db9df5291fa5b667764d9a
3278114689 <133768511+3278114689@users.noreply.github.com>
2023-08-03 11:47:20 +0800

    绝色

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('encouragement').addEventListener('click', function() {
-    const encouragements = ['熙熙宝贝，你是最棒的！', '你是最可爱的！', '你是张炜华的最爱!','天地因你失色'];
+    const encouragements = ['熙熙宝贝，你是最棒的！', '你是最可爱的！', '月色和雪色之间，你是第三种绝色!','天地因你失色'];
     const randomIndex = Math.floor(Math.random() * encouragements.length);
     document.getElementById('message').innerText = encouragements[randomIndex];
 });
