84e391505d1ca8f27fbe4e3f098e83f09567b3a0
3278114689 <133768511+3278114689@users.noreply.github.com>
2023-08-16 23:01:24 +0800

    重置

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('encouragement').addEventListener('click', function() {
-    const encouragements = ['熙熙宝贝，你是最棒的！', '你是最可爱的！', '月色和雪色之间，你是第三种绝色!','天地因你失色'];
+    const encouragements = ['哈哈哈，你是最棒的！', '你是叉叉的！', '月色和雪色之间，你是第三种牛马!','天地因你失色，因为你'];
     const randomIndex = Math.floor(Math.random() * encouragements.length);
     document.getElementById('message').innerText = encouragements[randomIndex];
 });
