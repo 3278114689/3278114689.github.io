47927ef4013f3555cecd222c49542155445d8b96
3278114689 <133768511+3278114689@users.noreply.github.com>
2023-10-08 09:42:10 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('encouragement').addEventListener('click', function() {
-    const encouragements = ['哈哈哈，好听！', '杀手皇后！', '月色和雪色之间，你是第三种牛马!','天地因你失色，因为你'];
+    const encouragements = ['哈哈哈，好听！', '杀手皇后！', '老实听完','只要活得精彩,我不在乎活多久'];
     const randomIndex = Math.floor(Math.random() * encouragements.length);
     document.getElementById('message').innerText = encouragements[randomIndex];
 });
