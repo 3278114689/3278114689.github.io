1ad72b7d6bab8d378be3505728fdf9504a62bb04
3278114689 <133768511+3278114689@users.noreply.github.com>
2023-10-08 09:36:16 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('encouragement').addEventListener('click', function() {
-    const encouragements = ['哈哈哈，你是最棒的！', '你是叉叉的！', '月色和雪色之间，你是第三种牛马!','天地因你失色，因为你'];
+    const encouragements = ['哈哈哈，好听！', '杀手皇后！', '月色和雪色之间，你是第三种牛马!','天地因你失色，因为你'];
     const randomIndex = Math.floor(Math.random() * encouragements.length);
     document.getElementById('message').innerText = encouragements[randomIndex];
 });
