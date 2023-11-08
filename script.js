c86af7c1cf70ebef02ebe21113f872846e41c52c
3278114689 <133768511+3278114689@users.noreply.github.com>
2023-11-08 19:43:21 +0800

    re
    re

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('encouragement').addEventListener('click', function() {
-    const encouragements = ['生日快乐，瑞连', '金风玉露一相逢，便胜却人间无数，此生此夕，愿你年华如诗，岁岁芳华如故。', '雲卷雲舒，岁月轻筝过指，今日为你，愿锦时光里，繁花似锦，璀璨如你','星汉灿烂，今朝尤为你闪烁；岁月如歌，愿你颂中寻找永恒的美好。'];
+    const encouragements = ['自定义语句', '自定义语句', '自定义语句','自定义语句'];
     const randomIndex = Math.floor(Math.random() * encouragements.length);
     const messageElement = document.getElementById('message');
     messageElement.innerText = encouragements[randomIndex];
