bb1bda6f0806577eca8a71bdfef02371e8132377
3278114689 <133768511+3278114689@users.noreply.github.com>
2024-03-21 15:09:01 +0800

    字

--- scripts.js
+++ scripts.js
@@ -7,57 +7,57 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 image: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/rh.jpg', // 填入第一张图片的URL
                 music: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/Creep-Radiohead.mp3', // 填入第一首音乐的URL
-                text: 'LAOY'
+                text: 'Creep-Radiohead'
             },
             {
                 image: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/eag.jpg', // 确保路径正确
                 music: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/Hotel%20California-Eagles.mp3', // 确保路径正确
-                text: '这里是第二幅图片的描述'
+                text: 'Hotel California-Eagles'
             },
             {
                 image: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/STROKE.jpg', // 确保路径正确
                 music: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/I%27ll%20Try%20Anything%20Once%20.mp3', // 确保路径正确
-                text: '这里是第3幅图片的描述'
+                text: 'I’ll Try Anything Once'
             },
             {
                 image: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/SQU.jpg', // 确保路径正确
                 music: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/She%27ll%20Make%20You%20Cry-The%20Velvet%20Underground.mp3', // 确保路径正确
-                text: '这里是第4幅图片的描述'
+                text: 'She’ll Make You Cry-The Velvet Underground'
             },
             {
                 image: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/VELVET.jpg', // 确保路径正确
                 music: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/What%20Goes%20On-The%20Velvet%20Underground.mp3', // 确保路径正确
-                text: '这里是第5幅图片的描述'
+                text: 'What Goes On-The Velvet Underground'
             },
             {
                 image: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/BTS.jpg', // 确保路径正确
                 music: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/When%20I%27m%20Sixty%20Four-.mp3', // 确保路径正确
-                text: '这里是第6幅图片的描述'
+                text: 'When I’m Sixty Four-The Beatles'
             },
             {
                 image: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/DAY.jpg', // 确保路径正确
                 music: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/%C2%A1Viva%20La%20Gloria-Green%20Day.mp3', // 确保路径正确
-                text: '这里是第7幅图片的描述'
+                text: 'Viva La Gloria-Green Day'
             },
             {
                 image: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/br.jpg', // 确保路径正确
                 music: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/If-Bread.mp3', // 确保路径正确
-                text: '这里是第8幅图片的描述'
+                text: 'If-Bread'
             },
             {
                 image: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/GREEN.jpg', // 确保路径正确
                 music: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/Green%20is%20the%20Colour-Pink%20Floyd.mp3', // 确保路径正确
-                text: '这里是第9幅图片的描述'
+                text: 'Green is the Colour-Pink Floyd'
             },
             {
                 image: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/BOY.jpg', // 确保路径正确
                 music: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/Wouldn%27t%20It%20Be%20Nice-The%20Beach%20Boys.mp3', // 确保路径正确
-                text: '这里是第10幅图片的描述'
+                text: 'Wouldn’t It Be Nice-The Beach Boys'
             },
             {
                 image: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/sem.jpg', // 确保路径正确
                 music: 'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/My%20Hero-Sean%20Lennon%2CKarla%20Moheno.mp3', // 确保路径正确
-                text: '这里是第11幅图片的描述'
+                text: 'My Hero-Sean Lennon,Karla Moheno'
             },
             
             // 若要添加更多轨道，继续以相同方式添加对象
