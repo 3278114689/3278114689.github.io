b0ad1e6ec7871935c35ed68a533529ad1e8f954d
3278114689 <133768511+3278114689@users.noreply.github.com>
2024-04-01 15:33:03 +0800

    1

---
+++ page2/script.js
@@ -0,0 +1,52 @@
+// DOM
+const swiper = document.querySelector('#swiper');
+const like = document.querySelector('#like');
+const dislike = document.querySelector('#dislike');
+
+// constants
+const urls = [
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/rh.gif',
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/eag.gif',
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/sirong.gif',
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/newBTS.gif',
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/GREEN.gif',
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/her.gif',
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/r8.gif',
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/dabao.gif',
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/muse.gif',
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/Steven.gif',
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/luysi.gif',
+  'https://u696.oss-cn-hangzhou.aliyuncs.com/PE/cowboy.gif'
+];
+
+// variables
+let cardCount = 0;
+
+// functions
+function appendNewCard() {
+  const card = new Card({
+    imageUrl: urls[cardCount % 12],
+    onDismiss: appendNewCard,
+    onLike: () => {
+      like.style.animationPlayState = 'running';
+      like.classList.toggle('trigger');
+    },
+    onDislike: () => {
+      dislike.style.animationPlayState = 'running';
+      dislike.classList.toggle('trigger');
+    }
+  });
+  swiper.append(card.element);
+  cardCount++;
+
+  const cards = swiper.querySelectorAll('.card:not(.dismissing)');
+  cards.forEach((card, index) => {
+    card.style.setProperty('--i', index);
+  });
+}
+
+// first 5 cards
+for (let i = 0; i < 12; i++) {
+  appendNewCard();
+}
+
